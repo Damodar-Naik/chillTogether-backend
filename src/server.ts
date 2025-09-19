d5b50a1528b8e67ba2daa966737df66c5c2dcbfd
Damodar-Naik <82937522+Damodar-Naik@users.noreply.github.com>
2025-09-19 13:14:41 +0530

    added functiality to load videos based on the url entered by user.

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,29 @@
 import { time } from 'console';
+import { url } from 'inspector';
 import webSocket from 'ws';
 
 const ws = new webSocket.Server({ port: 8080 });
 
 type socketMessage = {
-    type: 'play' | 'pause' | 'seek' | 'connected';
+    type: 'loadUrl' | 'play' | 'pause' | 'seek' | 'connected';
     message: string;
+    jsonData?: string; // json string
 }
-let lastSeek = {
+let videoData = {
     time: 0,
-    currentTime: 0
+    currentTime: 0,
+    videoId: ''
 }
 ws.on('connection', (socket) => {
     console.log('a user connected');
-    
+    if (!videoData.currentTime) {
+        videoData.currentTime = Date.now();
+    }
+
     const data: socketMessage = {
         type: 'connected',
-        message: JSON.stringify(lastSeek)
+        message: 'Welcome new client',
+        jsonData: JSON.stringify(videoData)
     }
     socket.send(JSON.stringify(data));
 
@@ -24,10 +31,15 @@ ws.on('connection', (socket) => {
         console.log('received: %s', message);
         const parsedMessage = JSON.parse(message.toString()) as socketMessage;
         if (['seek', 'play'].includes(parsedMessage.type)) {
-            lastSeek = {
+            videoData = {
                 time: Number(parsedMessage.message),
-                currentTime: Date.now()
+                currentTime: Date.now(),
+                videoId: videoData.videoId
             }
+        }
+
+        if (parsedMessage.type == 'loadUrl') {
+            videoData = parsedMessage.jsonData ? JSON.parse(parsedMessage.jsonData) : videoData;            
         }
 
         ws.clients.forEach((client) => {
