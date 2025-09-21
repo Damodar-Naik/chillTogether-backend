64ab166a10813aa483a6b1071d64d712f3b3c447
Damodar-Naik <82937522+Damodar-Naik@users.noreply.github.com>
2025-09-21 13:19:31 +0530

    keep users in sync

--- src/server.ts
+++ src/server.ts
@@ -9,10 +9,17 @@ type socketMessage = {
     message: string;
     jsonData?: string; // json string
 }
-let videoData = {
+type VideoDataType = {
+    time: number;
+    currentTime: number;
+    videoId: string;
+    status: 'playing' | 'paused' | 'seek' | 'buffering' | 'ended';
+}
+let videoData: VideoDataType = {
     time: 0,
     currentTime: 0,
-    videoId: ''
+    videoId: '',
+    status: 'paused' // playing, paused, seek, buffering, ended
 }
 ws.on('connection', (socket) => {
     console.log('a user connected');
@@ -30,16 +37,18 @@ ws.on('connection', (socket) => {
     socket.on('message', (message) => {
         console.log('received: %s', message);
         const parsedMessage = JSON.parse(message.toString()) as socketMessage;
-        if (['seek', 'play'].includes(parsedMessage.type)) {
-            videoData = {
-                time: Number(parsedMessage.message),
-                currentTime: Date.now(),
-                videoId: videoData.videoId
-            }
+        if (['seek', 'play', 'pause'].includes(parsedMessage.type)) {
+            videoData = parsedMessage.jsonData ? JSON.parse(parsedMessage.jsonData) : videoData;
         }
 
         if (parsedMessage.type == 'loadUrl') {
-            videoData = parsedMessage.jsonData ? JSON.parse(parsedMessage.jsonData) : videoData;            
+            videoData = parsedMessage.jsonData ? JSON.parse(parsedMessage.jsonData) : videoData;
+            videoData = {
+                time: 0,
+                currentTime: Date.now(),
+                videoId: videoData.videoId,
+                status: 'playing'
+            }
         }
 
         ws.clients.forEach((client) => {
